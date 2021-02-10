31ed3341c2e015af87282c09c1b48e8b4cdf36cc
byx2000 <416982860@qq.com>
2021-02-10 19:31:10 +0800

    添加“axios-wrapper”

---
+++ axios-wrapper/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{data}}
+  </div>
+</template>
+
+<script>
+import request from './network/request'
+
+export default {
+  name: 'App',
+  created() {
+    console.log("create")
+    request({
+      url: '/home/multidata'
+    }).then(res => {
+      this.data = res
+    })
+  },
+  data() {
+    return {
+      data: 'hello'
+    }
+  }
+}
+</script>
