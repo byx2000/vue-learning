d873163a6f836dca00ebb403507391e1dfe0965a
byx2000 <416982860@qq.com>
2021-02-25 10:31:28 +0800

    添加“vue-bootstrap”

---
+++ vue-bootstrap/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col class="c1" v-for="(item, index) in items" :key="index" cols="12" sm="6" md="4" lg="3">
+          {{item}}
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col class="c2" v-for="(item, index) in items" :key="index" cols="4">
+          {{item}}
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <div class="d-none d-xl-block">
+      你好
+    </div>
+
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      items: ['手机', '数码', '电子', '服装', '食品', '游戏', '休闲', '旅游']
+    }
+  }
+}
+</script>
+
+<style scoped>
+.c1 {
+  background: red;
+  color: white;
+}
+
+.c2 {
+  background: blue;
+  color: white;
+}
+</style>
